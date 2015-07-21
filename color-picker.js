9d8f77517e6604c5dae3a06e09a279b5b1c63774
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2015-07-21 17:43:47 -0300

    Update color-picker.js

--- color-picker.js
+++ color-picker.js
@@ -16,4 +16,39 @@ angular.module('ngColorPicker', [])
         }
     };
 
+})
+.directive('colorpicker', function() {
+    var defaultColors =  [
+        '#A0522D',
+        '#CD5C5C',
+        '#FF4500',
+        '#DC143C',
+        '#FF8C00',
+        '#C71585',
+        '#00FF00',
+        '#33CCFF',
+        '#FFFF00',
+        '#708090',
+        '#00C5CD',
+        '#CDCD00',
+        '#EE6363',
+        '#7D26CD',
+        '#363636'
+    ];
+    return {
+        scope: {
+            ngModel: '='
+        },
+        restrict: 'AE',
+        template: '<ul><li ng-repeat="color in colors track by $index" ng-class="{selected: (color===ngModel)}" ng-click="pick(color)" ng-style="{\'background-color\':color};"></li></ul>',
+        link: function (scope) {
+            scope.colors = defaultColors;
+            scope.ngModel = scope.ngModel || scope.colors[0];
+
+            scope.pick = function (color) {
+                scope.ngModel = color;
+            };
+        }
+    };
+
 });
