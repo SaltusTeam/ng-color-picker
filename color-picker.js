6e23866c2c8d02299406619b09a32318a664c674
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2015-07-21 17:34:24 -0300

    Update color-picker.js

--- color-picker.js
+++ color-picker.js
@@ -1,17 +1,17 @@
 angular.module('ngColorPicker', [])
-.directive('ngColorPicker', function() {
+.directive('colorpickerDinamic', function() {
     return {
         scope: {
-            selected: '=',
+            ngModel: '=',
             colors: '='
         },
         restrict: 'AE',
-        template: '<ul><li ng-repeat="color in colors track by $index" ng-class="{selected: (color===selected)}" ng-click="pick(color)" ng-style="{\'background-color\':color};"></li></ul>',
+        template: '<ul><li ng-repeat="color in colors track by $index" ng-class="{selected: (color===ngModel)}" ng-click="pick(color)" ng-style="{\'background-color\':color};"></li></ul>',
         link: function (scope) {
-            scope.selected = scope.selected || scope.colors[0];
+            scope.ngModel = scope.ngModel || scope.colors[0];
 
             scope.pick = function (color) {
-                scope.selected = color;
+                scope.ngModel = color;
             };
         }
     };
